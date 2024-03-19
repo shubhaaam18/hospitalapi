bb16b86a503634c9a62ad415e7eb4d7a318eba89
shubham pardesi <shubhampardesi8@gmail.com>
2024-03-19 16:16:15 +0530

    third commit

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const app = express();
 
-
+// Define the port number
 const port = 6500;
-const db = require('./config/mongoose')
 
-//Used for Session Cookie
+// Import the database connection from './config/mongoose'
+const db = require('./config/mongoose');
+
+// Import passport and passport JWT strategy
 const passport = require('passport');
-const passportJWT = require('./config/passport-jwt-strategy')
+const passportJWT = require('./config/passport-jwt-strategy');
 
+// Middleware to parse incoming requests with URL-encoded payloads
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({ extended: true }))
-
+// Initialize passport middleware
 app.use(passport.initialize());
 
-// Use express router
-app.use('/', require('./routes/index'))
+// Use express router for routing
+app.use('/', require('./routes/index'));
 
+// Start the server and listen on the specified port
 app.listen(port, function (error) {
     if (error) {
         console.log(`Error in running the Server. Error is : ${error}`);
         return;
     }
     console.log(`connected to http://localhost:${port}`);
-})
+});
